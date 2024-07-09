362ea0b55ad934d3b28ad4c9a8dc58b09c182d86
조주희 <carla_cho@naver.com>
2024-07-09 14:33:42 +0900

    feat: 헤더 버튼 클릭 시 메인으로 이동 구현

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { Ic24 } from "../../assets/png/icon";
+import { Ic24 } from "../../assets/svg/icon";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+    const navigate = useNavigate();
+
+    const handleClickLogoButton = () => {
+        navigate("/");
+    };
+
     return (
         <HeaderWrapper>
             <HeaderBodyWrapper>
-                <HeaderTitle>검수</HeaderTitle>
-                <Icon src={Ic24} alt="icon" />
+                <HeaderLogoButton onClick={handleClickLogoButton}>
+                    <HeaderTitle>신검</HeaderTitle>
+                    <StyledIc24 />
+                </HeaderLogoButton>
             </HeaderBodyWrapper>
         </HeaderWrapper>
     );
@@ -28,13 +37,20 @@ const HeaderBodyWrapper = styled.div`
     height: 7rem;
 `;
 
+const HeaderLogoButton = styled.button`
+    display: flex;
+    align-items: center;
+
+    gap: 0.7rem;
+`;
+
 const HeaderTitle = styled.div`
     padding-left: 1rem;
 
-    font-size: 2.75rem;
+    font-size: 4rem;
 `;
 
-const Icon = styled.img`
+const StyledIc24 = styled(Ic24)`
     width: 4rem;
     height: 4rem;
 `;
