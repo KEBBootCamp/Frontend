feaf79b3284a328069195de330159e02a72c1c6c
조주희 <carla_cho@naver.com>
2024-07-09 16:41:59 +0900

    feat: 전문가리스트 페이지  퍼블리싱

---
+++ src/pages/ExpertList.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Common/Header";
+import { IcUser } from "../assets/svg/icon";
+
+function ExpertList() {
+    return (
+        <ExpertListWrapper>
+            <Header />
+            <ExpertBodyWrapper>
+                <ExpertListBoxWrapper>
+                    <ExpertLeftBox>
+                        <StyledIcUser />
+                    </ExpertLeftBox>
+                    <ExpertRightBox>
+                        <ExpertRightBoxName>이름 : 홍길동</ExpertRightBoxName>
+                        <ExpertRightBoxPhoneNum>연락처 : 010-1111-1234</ExpertRightBoxPhoneNum>
+                        <ExpertRightBoxIntro>한 줄 소개 : 현대차 검수에 특화된 전문가입니다!</ExpertRightBoxIntro>
+                    </ExpertRightBox>
+                </ExpertListBoxWrapper>
+                <ExpertListBoxWrapper>
+                    <ExpertLeftBox>
+                        <StyledIcUser />
+                    </ExpertLeftBox>
+                    <ExpertRightBox>
+                        <ExpertRightBoxName>이름 : 뽀로로</ExpertRightBoxName>
+                        <ExpertRightBoxPhoneNum>연락처 : 010-2222-1234</ExpertRightBoxPhoneNum>
+                        <ExpertRightBoxIntro>한 줄 소개 : 편하게 연락주십쇼!</ExpertRightBoxIntro>
+                    </ExpertRightBox>
+                </ExpertListBoxWrapper>
+                <ExpertListBoxWrapper>
+                    <ExpertLeftBox>
+                        <StyledIcUser />
+                    </ExpertLeftBox>
+                    <ExpertRightBox>
+                        <ExpertRightBoxName>이름 : 훌랄라</ExpertRightBoxName>
+                        <ExpertRightBoxPhoneNum>연락처 : 010-3333-1234</ExpertRightBoxPhoneNum>
+                        <ExpertRightBoxIntro>한 줄 소개 : 언제든지 문의 환영합니다!</ExpertRightBoxIntro>
+                    </ExpertRightBox>
+                </ExpertListBoxWrapper>
+            </ExpertBodyWrapper>
+        </ExpertListWrapper>
+    );
+}
+
+export default ExpertList;
+
+const ExpertListWrapper = styled.div`
+    width: 100vw;
+    height: 100dvh;
+    padding: 2rem;
+`;
+
+const ExpertBodyWrapper = styled.div`
+    border-radius: 1rem;
+    margin-top: 3rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+`;
+
+const ExpertListBoxWrapper = styled.div`
+    width: 100%;
+
+    font-size: 1.5rem;
+    background-color: rgb(245, 245, 247);
+
+    display: flex;
+    align-items: center;
+
+    cursor: pointer;
+    transition: all 0.3s ease 0s;
+`;
+
+const ExpertLeftBox = styled.div`
+    width: 10rem;
+    height: 10rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    background-color: #bcbcbc;
+`;
+
+const StyledIcUser = styled(IcUser)`
+    width: 6rem;
+    height: 6rem;
+`;
+const ExpertRightBox = styled.div`
+    width: calc(100% - 10rem);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+
+    background-color: rgb(245, 245, 247);
+`;
+
+const ExpertRightBoxName = styled.div`
+    width: 20rem;
+`;
+const ExpertRightBoxPhoneNum = styled.div`
+    width: 20rem;
+`;
+const ExpertRightBoxIntro = styled.div`
+    width: 20rem;
+`;
