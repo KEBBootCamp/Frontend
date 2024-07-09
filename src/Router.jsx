7fdcdc1215dd6c0e76e314f653f3de6080fc1d36
조주희 <carla_cho@naver.com>
2024-07-09 17:16:10 +0900

    fix: 라우터 경로 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ExpertList from "./pages/ExpertList";
+import Login from "./pages/Login";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/expert-list" element={<ExpertList />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );
