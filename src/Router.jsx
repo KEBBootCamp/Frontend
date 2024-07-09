610124fa73f59f6324751d0fa5fb1a661e3d75fc
조주희 <carla_cho@naver.com>
2024-07-09 09:24:29 +0900

    fix: 라우터 import 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/MainPage/Home";
+import Home from "./pages/Home";
 
 function Router() {
     return (
