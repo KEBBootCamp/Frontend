8d3d8f9f2107721ba5c8234f9315c7ad23d1cb1c
조주희 <carla_cho@naver.com>
2024-07-10 16:48:39 +0900

    add: svg파일 추가 후 icon.js 수정

--- src/assets/svg/icon.js
+++ src/assets/svg/icon.js
@@ -1,5 +1,6 @@
 import Ic24 from "./ic_24.svg?react";
 import IcUser from "./ic_user.svg?react";
 import IcMyProfile from "./ic_profile.svg?react";
+import IcBack from "./ic_back.svg?react";
 
-export { Ic24, IcUser, IcMyProfile };
+export { Ic24, IcUser, IcMyProfile, IcBack };
