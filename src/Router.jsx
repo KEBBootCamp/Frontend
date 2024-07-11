e94d01c98778801842c23edca1440fc34d30c950
조주희 <carla_cho@naver.com>
2024-07-11 15:04:21 +0900

    fix: 라우터에 MyPage 페이지  연결 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ExpertList from "./pages/ExpertList";
 import Login from "./pages/Login";
+import Mypage from "./pages/Mypage";
 
 function Router() {
     return (
@@ -11,6 +12,7 @@ function Router() {
                 <Route path="/" element={<Home />} />
                 <Route path="/expert-list" element={<ExpertList />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/my-page" element={<Mypage />} />
             </Routes>
         </BrowserRouter>
     );
