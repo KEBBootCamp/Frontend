86af8359a769a6070f78d1860a38aa71c8343327
조주희 <carla_cho@naver.com>
2024-07-12 17:29:22 +0900

    add: 체크표시 svg 파일 추가

--- src/assets/svg/icon.js
+++ src/assets/svg/icon.js
@@ -2,5 +2,6 @@ import Ic24 from "./ic_24.svg?react";
 import IcUser from "./ic_user.svg?react";
 import IcMyProfile from "./ic_profile.svg?react";
 import IcBack from "./ic_back.svg?react";
+import IcChecked from "./ic_checked.svg?react";
 
-export { Ic24, IcUser, IcMyProfile, IcBack };
+export { Ic24, IcUser, IcMyProfile, IcBack, IcChecked };
