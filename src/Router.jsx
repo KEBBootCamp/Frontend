a258ada487a559f6113213a63f339bbb1f9e33da
조주희 <carla_cho@naver.com>
2024-07-12 17:30:08 +0900

    fix: 라우터에 회원가입 페이지 경로 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home";
 import ExpertList from "./pages/ExpertList";
 import Login from "./pages/Login";
 import Mypage from "./pages/Mypage";
+import SignUpUsage from "./components/SignUp/SignUpUsage";
+import SignUp1 from "./components/SignUp/SignUp1";
+import SignUp2 from "./components/SignUp/SignUp2";
+import SignUpDone from "./components/SignUp/SignUpDone";
 
 function Router() {
     return (
@@ -13,6 +17,10 @@ function Router() {
                 <Route path="/expert-list" element={<ExpertList />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/my-page" element={<Mypage />} />
+                <Route path="/sign-up-usage" element={<SignUpUsage />} />
+                <Route path="/sign-up-1" element={<SignUp1 />} />
+                <Route path="/sign-up-2" element={<SignUp2 />} />
+                <Route path="/sign-up-done" element={<SignUpDone />} />
             </Routes>
         </BrowserRouter>
     );
