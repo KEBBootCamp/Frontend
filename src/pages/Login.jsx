6ac012c3cecbd5a013710e60697375112f521277
조주희 <carla_cho@naver.com>
2024-07-25 11:01:17 +0900

    fix: 아이디&비밀번호 5자리 이상이어야 로그인 버튼 활성화 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Header from "../components/Common/Header";
+import Header from "../components/common/Header";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
     const navigate = useNavigate();
+    const [isSatisfied, setIsSatisfied] = useState(false);
+    const [id, setId] = useState("");
+    const [pwd, setPwd] = useState("");
+
+    const userId = (e) => {
+        setId(e.target.value);
+    };
+    const userPwd = (e) => {
+        setPwd(e.target.value);
+    };
+
+    useEffect(() => {
+        if (id.length >= 5 && pwd.length >= 5) {
+            setIsSatisfied(true);
+        } else {
+            setIsSatisfied(false);
+        }
+    }, [id, pwd]);
 
     const handleClickLoginButton = () => {
         navigate("/");
     };
+
+    const handleClickNotMemberButton = () => {
+        navigate("/sign-up-usage");
+    };
+
     return (
         <LoginWrapper>
             <Header />
@@ -16,18 +39,20 @@ function Login() {
                 <LoginTextWrapper>
                     <LoginText>로그인</LoginText>
                 </LoginTextWrapper>
-                <LoginBox>
-                    <LoginBoxId>
-                        <LoginInput type="text" placeholder="아이디" />
-                    </LoginBoxId>
-                    <LoginBoxPwd>
-                        <LoginInput type="text" placeholder="비밀번호" />
-                    </LoginBoxPwd>
-                </LoginBox>
+                <LoginBoxContainer>
+                    <LoginBoxWrapper>
+                        <LoginBox type="text" placeholder="아이디" value={id} onChange={userId} />
+                    </LoginBoxWrapper>
+                    <LoginBoxWrapper>
+                        <LoginBox type="password" placeholder="비밀번호" value={pwd} onChange={userPwd} />
+                    </LoginBoxWrapper>
+                </LoginBoxContainer>
             </BoxContainer>
             <LoginContainer>
-                <LoginButton onClick={handleClickLoginButton}>로그인</LoginButton>
-                <NotMember>회원이 아닙니다.</NotMember>
+                <LoginButton onClick={handleClickLoginButton} isSatisfied={isSatisfied}>
+                    로그인
+                </LoginButton>
+                <NotMember onClick={handleClickNotMemberButton}>회원이 아닙니다.</NotMember>
             </LoginContainer>
         </LoginWrapper>
     );
@@ -63,38 +88,47 @@ const LoginTextWrapper = styled.div`
     flex-direction: column;
     align-items: center;
 
-    font-weight: 400;
     font-size: 4rem;
 `;
 
-const LoginBox = styled.div`
-    width: 70%;
-
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-`;
-
-const LoginBoxId = styled.div``;
-
-const LoginBoxPwd = styled.div``;
-
-const LoginInput = styled.input`
+const LoginBoxWrapper = styled.div`
     width: 100%;
     height: 4.5rem;
 
-    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 0.5rem;
+`;
+
+const LoginBox = styled.input`
+    width: 90%;
+    height: 4.3rem;
+    padding: 0 1rem;
+
     border: 0.05rem solid #9e9e9e;
     border-radius: 0.5rem;
 `;
 
 const LoginText = styled.div``;
 
+const LoginBoxContainer = styled.div`
+    width: 70%;
+
+    margin-top: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+`;
 const LoginContainer = styled.div`
     width: 100%;
+
     display: flex;
     flex-direction: column;
     align-items: center;
+
     margin-top: 4rem;
 `;
 
@@ -102,13 +136,13 @@ const LoginButton = styled.button`
     width: 100%;
     height: 6rem;
 
-    font-weight: 600;
+    font-weight: 300;
     font-size: 1.8rem;
 
     margin-top: 4rem;
 
     border-radius: 1rem;
-    background-color: #4784ff;
+    background-color: ${({ isSatisfied }) => (isSatisfied ? "#4784ff" : "#cde9f4")};
     color: white;
 
     cursor: pointer;
@@ -117,7 +151,6 @@ const LoginButton = styled.button`
 const NotMember = styled.button`
     color: rgb(158, 158, 158);
 
-    font-weight: 400;
     font-size: 1.4rem;
     text-decoration: underline;
     line-height: 150%;
