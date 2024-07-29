8c6ee270db68064264b2d26f22bc8e810b48b912
조주희 <carla_cho@naver.com>
2024-07-29 10:57:54 +0900

    delete: Common->common 폴더 이름 변경을 위한 삭제

--- src/components/Common/MatchingModalPortal.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-
-function MatchingModalPortal({ children }) {
-    const el = document.getElementById("matching-modal");
-    return ReactDOM.createPortal(children, el);
-}
-
-export default MatchingModalPortal;
