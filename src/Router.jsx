569298ace676a2131fd1cae5c5751b9d522c1252
조주희 <carla_cho@naver.com>
2024-07-29 14:51:40 +0900

    fix: Router.jsx 설정 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ExpertList from "./pages/ExpertList";
+import ExpertDetail from "./pages/ExpertDetail";
 import Login from "./pages/Login";
-import Mypage from "./pages/Mypage";
+import ExpertMypage from "./pages/ExpertMypage";
+import UserMypage from "./pages/UserMypage";
 import SignUpUsage from "./components/SignUp/SignUpUsage";
 import SignUp1 from "./components/SignUp/SignUp1";
 import SignUp2 from "./components/SignUp/SignUp2";
@@ -17,13 +19,14 @@ function Router() {
                 <Route path="/" element={<Home />} />
                 <Route path="/expert-list" element={<ExpertList />} />
                 <Route path="/login" element={<Login />} />
-
-                <Route path="/my-page" element={<Mypage />} />
+                <Route path="/user-my-page" element={<UserMypage />} />
+                <Route path="/expert-my-page" element={<ExpertMypage />} />
                 <Route path="/sign-up-usage" element={<SignUpUsage />} />
                 <Route path="/sign-up-1" element={<SignUp1 />} />
                 <Route path="/sign-up-2" element={<SignUp2 />} />
                 <Route path="/sign-up-done" element={<SignUpDone />} />
                 <Route path="/loading" element={<Loading />} />
+                <Route path="/expert-detail" element={<ExpertDetail />} />
             </Routes>
         </BrowserRouter>
     );
