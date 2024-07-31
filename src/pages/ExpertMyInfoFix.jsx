482f922122c090d11c80a7b8db2a1cf18cfef06c
조주희 <carla_cho@naver.com>
2024-07-31 16:43:58 +0900

    fix: 전문가 마이페이지=> 내정보수정 스타일 수정

--- src/pages/ExpertMyInfoFix.jsx
+++ src/pages/ExpertMyInfoFix.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled } from "styled-components";
+import styled from "styled-components";
 import MypageHeader from "../components/common/MypageHeader";
 
 function ExpertMyInfoFix() {
@@ -14,17 +14,17 @@ function ExpertMyInfoFix() {
                     </ExpertWrapper>
                     <ExpertWrapper>
                         <ExpertText>연락처</ExpertText>
-                        <ExpertPhoneNum type="text" placeholder="010-0000-0000" />
+                        <ExpertPhoneNum type="text" placeholder="010-1234-5678" />
                     </ExpertWrapper>
                     <ExpertWrapper>
                         <ExpertText>한 줄 소개</ExpertText>
-                        <ExpertIntro type="text" placeholder="현대차 검수에 특화된 전문가입니다. 연락 주세요!" />
+                        <ExpertIntro placeholder="현대차 검수에 특화된 전문가입니다. 연락 주세요!" />
                     </ExpertWrapper>
                 </ToggleExpertBox>
+                <ExpertMyinfoBox>
+                    <FixDoneButton>저장하기</FixDoneButton>
+                </ExpertMyinfoBox>
             </ToggleBoxContainer>
-            <ExpertMyinfoBox>
-                <FixDoneButton>저장하기</FixDoneButton>
-            </ExpertMyinfoBox>
         </ExpertMyInfoFixWrapper>
     );
 }
@@ -33,71 +33,73 @@ export default ExpertMyInfoFix;
 
 const ExpertMyInfoFixWrapper = styled.div`
     width: 100vw;
-    height: 100dvh;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
     background-color: rgb(245, 245, 247);
 `;
 
-const ExpertMyinfoBox = styled.div`
-    display: flex;
-
-    padding: 1.3rem;
-
-    font-size: 2rem;
-`;
-
 const ToggleBoxContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-
-    border-radius: 1rem;
+    width: 100%;
+    padding: 2rem;
 
     background-color: rgb(245, 245, 247);
 `;
 
 const ToggleExpertBox = styled.div`
-    width: 30rem;
-    height: 25rem;
+    height: 30rem;
 
     display: flex;
     flex-direction: column;
-    justify-content: center;
     gap: 2rem;
 
+    padding: 1.5rem;
     border-radius: 1rem;
-
-    padding: 0 2rem;
-    margin-top: 4rem;
-
-    font-size: 2rem;
     background-color: white;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const ExpertWrapper = styled.div`
-    height: 4.5rem;
-
     display: flex;
-    align-items: center;
-    justify-content: space-between;
+    flex-direction: column;
+    gap: 1rem;
 `;
 
-const ExpertText = styled.div``;
+const ExpertText = styled.label`
+    font-size: 1.5rem;
+    font-weight: bold;
+`;
 
 const ExpertCareer = styled.input`
-    width: 11rem;
-    height: 2rem;
+    padding: 0.5rem;
+
+    font-size: 1.2rem;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
 `;
 
 const ExpertPhoneNum = styled.input`
-    width: 11rem;
-    height: 2rem;
+    padding: 0.5rem;
+
+    font-size: 1.2rem;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
 `;
 
 const ExpertIntro = styled.textarea`
-    height: 5rem;
+    height: 6rem;
+
+    padding: 0.5rem;
+
+    font-size: 1.2rem;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
 `;
+
+const ExpertMyinfoBox = styled.div``;
 
 const FixDoneButton = styled.button`
     width: 8rem;
@@ -106,7 +108,8 @@ const FixDoneButton = styled.button`
     border-radius: 0.7rem;
     font-size: 1.5rem;
 
-    margin-left: 25rem;
+    margin-top: 2rem;
+    margin-left: 24rem;
     background-color: rgb(4, 41, 63);
     color: white;
 `;
